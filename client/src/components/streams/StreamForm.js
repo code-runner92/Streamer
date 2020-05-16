48f7f28725bdbf242d5b5597ad3110d1b1569dfc
arkejgit <arkej@roberts-macbook-air.home>
2020-05-16 09:45:35 +0200

    Refactor - move form to separate component

---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamForm extends React.Component {
+	
+	renderError({ error, touched }){
+		if (touched && error){
+			return(
+				<div className="ui error message">
+					<div className="header">{ error }</div>
+				</div>
+			);
+		}
+	}
+
+	renderInput = ({ input, label, meta }) => {
+		const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+		return (
+			<div className={ className }>
+				<label>{ label }</label>
+				<input {...input} autoComplete="off" />
+				<div>{ this.renderError(meta) }</div>
+			</div>
+		)
+	}
+
+	onSubmit = formValues => {
+		this.props.onSubmit(formValues);
+	}
+
+	render(){
+		return (
+			<form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+				<Field name="title" label="Title:" component={this.renderInput} />
+				<Field name="description" label="Desciption:" component={this.renderInput} />
+				<button className="ui button primary">Submit</button>
+			</form>
+		);
+	}
+}
+
+const validate = (formValues) => {
+	const errors = {};
+
+	if (!formValues.title){
+		errors.title = 'You must enter the title';
+	}
+
+	if (!formValues.description){
+		errors.description = 'You must enter the description';
+	}
+
+	return errors;
+}
+
+export default reduxForm({
+	form: 'streamForm',
+	validate,
+})(StreamForm);
